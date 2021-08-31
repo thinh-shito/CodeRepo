e35a125fba46f0a6fe75cb99ff737167be05b7ad
CodeRepo <dothinhtpr247@gmail.com>
2021-08-31 22:18:08 +0700

    update fist quicksort

---
+++ Quicksort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void QuickSort(int *a, int left, int right)
+{
+	int x=a[(left+right)/2];
+	int i=left;
+	int j=right;
+		while (i<j)
+		{
+			while(a[i]<x) i++;
+			while(a[j]>x) j--;
+			if(i<=j)
+				{
+					swap(a[i],a[j]);
+					i++;
+					j--;
+				}
+		}	
+	if(j>left)	QuickSort(a,left,j);
+	if(i<right)	QuickSort(a,i,right);
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int *a =new int [n];
+	for(int i=0; i<n; i++)
+	cin>>a[i];
+	QuickSort(a,0,n-1);
+		for(int i=0; i<n; i++)
+		cout<<a[i]<<endl;
+	
+	return 0;
+}
